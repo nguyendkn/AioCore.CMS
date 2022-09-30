d8ca3888c313aead59f0fc113d152ef1a11cd2ba
Dao Khoi Nguyen <nguyendk.on@gmail.com>
2022-09-30 16:48:20 +0700

    chore: completed bind data to category

--- src/AioCore.Shared/Extensions/CollectionExtensions.cs
+++ src/AioCore.Shared/Extensions/CollectionExtensions.cs
@@ -5,6 +5,7 @@ public static class CollectionExtensions
     public static List<T> Slice<T>(this List<T>? items, int start, int count)
     {
         if (items is null || start > count) return default!;
+        count = items.Count > count ? count : items.Count;
         var i = 0;
         while (i < count)
         {
